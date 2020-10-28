b5a055255ff65c7f6604c858b28249e621b4c3f5
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-10-28 05:50:55 +0300

    implement router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignupForm';
 import './App.css';
+import Appointments from './containers/Appointments';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/signup" component={SignUpForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/appointments" component={Appointments} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
