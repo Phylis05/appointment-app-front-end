c63b21fd5440e0c4e77cd30c19cae28f97e5d2c3
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-11-02 16:54:02 +0300

    update API url

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,16 +1,16 @@
+/* eslint-disable no-template-curly-in-string */
 import axios from 'axios';
 import * as reducerAction from '../actions/index';
 
-const HealthApi = (() => {
+const HospitalAppointment = (() => {
   const signUpUser = user => async dispatch => {
     try {
       const data = await axios.post(
-        'http://localhost:3000/signup',
+        'https://wicked-goosebumps-55914.herokuapp.com/signup',
         {
           users: {
             email: user.email,
             name: user.name,
-            contact_number: user.contact_number,
             password: user.password,
             password_confirmation: user.password_confirmation,
           },
@@ -26,7 +26,7 @@ const HealthApi = (() => {
   const loginUser = user => async dispatch => {
     try {
       const data = await axios.post(
-        'http://localhost:3000/auth/login',
+        'https://wicked-goosebumps-55914.herokuapp.com/auth/login',
         {
           users: {
             email: user.email,
@@ -45,7 +45,7 @@ const HealthApi = (() => {
   const getAllDoctors = () => async dispatch => {
     const token = localStorage.getItem('token');
     try {
-      const data = await axios.get('http://localhost:3000/v1/doctors',
+      const data = await axios.get('https://wicked-goosebumps-55914.herokuapp.com/doctors',
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -57,10 +57,25 @@ const HealthApi = (() => {
     }
   };
 
+  const getDoctor = () => async dispatch => {
+    const token = localStorage.getItem('token');
+    try {
+      const data = await axios.get('https://wicked-goosebumps-55914.herokuapp.com/doctors/${id}',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      dispatch(reducerAction.getDoctor(data.data));
+    } catch (error) {
+      dispatch(reducerAction.formErrors(error.response.data.message));
+    }
+  };
+
   const getAllAppointments = () => async dispatch => {
     const token = localStorage.getItem('token');
     try {
-      const data = await axios.get('http://localhost:3000/v1/measurements',
+      const data = await axios.get('https://wicked-goosebumps-55914.herokuapp.com/appointments',
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -72,48 +87,13 @@ const HealthApi = (() => {
     }
   };
 
-  // const addMeasurement = measure => async dispatch => {
-  //   const token = localStorage.getItem('token');
-  //   const config = {
-  //     headers: { Authorization: `Bearer ${token}` },
-  //   };
-
-  //   const measurements = {
-  //     value: measure.value,
-  //     date: measure.date,
-  //     measure_category_id: measure.measure_category_id,
-  //   };
-  //   try {
-  //     const data = await axios.post('https://nameless-temple-95690.herokuapp.com/api/v1/measurements', measurements, config);
-  //     dispatch(reducerAction.addMeasurements(data.measure));
-  //   } catch (error) {
-  //     dispatch(reducerAction.formErrors(error.response.data.message));
-  //   }
-  // };
-
-  // const getAllMeasurementId = id => async dispatch => {
-  //   const token = localStorage.getItem('token');
-  //   try {
-  //     const data = await axios.get(`https://nameless-temple-95690.herokuapp.com/api/v1/measurements/${id}`,
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       });
-  //     dispatch(reducerAction.getMeasuresId(data.data));
-  //   } catch (error) {
-  //     dispatch(reducerAction.formErrors(error.response.data.message));
-  //   }
-  // };
-
   return {
     signUpUser,
     loginUser,
     getAllAppointments,
     getAllDoctors,
-    // getAllMeasurements,
-    // getAllMeasurementId,
+    getDoctor,
   };
 })();
 
-export default HealthApi;
+export default HospitalAppointment;
